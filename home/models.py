231bdfa7e415b787959c974f2269761984d94a16
KoushikBiswas909 <koushikb909@gmail.com>
2022-07-13 19:10:46 +0530

    first commit

---
+++ home/models.py
@@ -0,0 +1,11 @@
+from unicodedata import name
+from django.db import models
+
+# Create your models here.
+
+
+class Names(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
---
+++ home/seed.py
@@ -0,0 +1,11 @@
+from unicodedata import name
+from .models import Names
+from faker import Faker
+fake = Faker()
+
+
+def seed_db(n):
+    for i in range(0, n):
+        Names.objects.create(
+            name=fake.name()
+        )
